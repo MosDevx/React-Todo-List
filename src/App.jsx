ca2a219948962745373c0c355f6f1e6ff3604442
MosDevx <mosesk.wamae@gmail.com>
2022-12-16 12:11:59 +0300

    Made Todo App from Tutorial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TodoContainer from './Components/TodoContainer'
+import "./App.css"
+function App() {
+
+  return(
+    <TodoContainer/>
+  )
+}
+
+export default App
